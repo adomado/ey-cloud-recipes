315d14bcd92585bbedb79baf46d6470926e8d65c
makuchaku <makuchaku@adomado.com>
2011-10-20 15:44:45 +0530

    Mongodb can now run on x86 machine

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -5,7 +5,7 @@
 
 # Setup an arbiter on the db_master|solo as replica sets need another vote to properly failover.  If you have a Replica set > 3 nodes we don't set this up, you can tune this obviously.
 case node[:kernel][:machine]
-when "i686"
+#when "i686"
   # Do nothing, you should never run MongoDB in a i686/i386 environment it will damage your data.
 else
   if (['db_master','solo'].include?(@node[:instance_role]) &&  @node[:mongo_utility_instances].length < 3)
